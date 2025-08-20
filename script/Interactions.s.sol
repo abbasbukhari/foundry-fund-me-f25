aa73a289657aa769df02af4c6e06eaab8f321ef3
abbas <abbasbukhari2015@gmail.com>
2025-08-20 11:19:07 -0400

    feat: add gas tracking and interactions scripts for FundMe tests

---
+++ script/Interactions.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {FundMe} from "../src/FundMe.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract FundFundMe is Script {
+    function run() external {}
+}
+
+contract WithdrawFundMe is Script {}
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -12,6 +12,7 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
@@ -72,8 +73,13 @@ contract FundMeTest is Test {
         uint256 startingOwnerBalance = fundMe.getOwner().balance;
         uint256 startingFundMeBalance = address(fundMe).balance;
         // Act
+        uint256 gasStart = gasleft();
+        vm.txGasPrice(GAS_PRICE);
         vm.prank(fundMe.getOwner());
         fundMe.withdraw();
+        // uint256 gasEnd = gasleft();
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        // console.log("Gas used for withdrawal:", gasUsed);
         // Assert
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
         uint256 endingFundMeBalance = address(fundMe).balance;
